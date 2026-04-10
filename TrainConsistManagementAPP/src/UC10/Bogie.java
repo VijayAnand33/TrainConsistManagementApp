1821d64c66cf0805836efea658dd475dfdbe2532
VijayAnand33 <vijayanand8208@gmail.com>
2026-04-10 10:01:34 +0530

    UC10: Count Total Seats in Train (reduce)

---
+++ TrainConsistManagementAPP/src/UC10/Bogie.java
@@ -0,0 +1,29 @@
+package UC10;
+
+/*
+ * MODEL CLASS - Bogie
+ */
+
+public class Bogie {
+
+    private String name;
+    private int capacity;
+
+    public Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    @Override
+    public String toString() {
+        return name + " -> " + capacity;
+    }
+}
